10930b880f63fd126989b116a936006fef681ac6
zeeshan1203 <zeeshanshaik1203@gmail.com>
2022-01-20 18:24:59 +0530

    added & updated and modified

--- ami.tf
+++ ami.tf
@@ -49,3 +49,8 @@ resource "null_resource" "ansible-apply" {
     ]
   }
 }
+
+resource "aws_ami_from_instance" "ami" {
+  name               = "${var.COMPONENT}-${var.APP_VERSION}"
+  source_instance_id = aws_instance.ami-instance.id
+}
